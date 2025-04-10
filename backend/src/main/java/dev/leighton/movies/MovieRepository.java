a444705e0ad3ba4d892b9b6c0f7a1807cb462412
Leighton Coughlin <leighton.coughlin1@gmail.com>
2025-04-10 19:58:57 +0100

    got the backend displaying json to browser

---
+++ backend/src/main/java/dev/leighton/movies/MovieRepository.java
@@ -0,0 +1,8 @@
+package dev.leighton.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {}
