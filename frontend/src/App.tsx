25030330ef7bb3fbb758cbe0f47ec7fb4eaeaf3c
Leighton Coughlin <leighton.coughlin1@gmail.com>
2025-04-18 17:46:09 +0100

    setup frontend boilerplate

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-black h-screen flex justify-center items-center">
+        <h1 className="text-3xl">Hello world!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
